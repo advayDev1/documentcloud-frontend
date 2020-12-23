3b44d3311b58c681b1b3f0c840a73ec62adc1d41
Dylan Freedman <freedmand@gmail.com>
2020-12-23 22:00:02 +0000

    Add entity actuality

--- src/api/entity.js
+++ src/api/entity.js
@@ -5,14 +5,6 @@ import { Entities } from '@/structure/entity';
 import { page1, page2 } from './mockEntity';
 
 export async function getEntities(id, page = 1) {
-  if (page == 1) {
-    const data = page1;
-    return new Entities(data);
-  } else {
-    const data = page2;
-    return new Entities(data);
-  }
-
   // Returns annotations for the specified document
   const { data } = await session.get(
     apiUrl(queryBuilder(`documents/${id}/entities/`, { page }))
